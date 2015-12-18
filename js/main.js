b1a60ec8d54a6c05d35ade2ff390672302ef9b64
IKEA <jellynina@gmail.com>
2015-12-19 01:30:51 +0800

    Gallery with project list

--- js/main.js
+++ js/main.js
@@ -49,6 +49,30 @@ $('.picture').each( function() {
   });
 });
 
+// project-list pager
+$('.paginated').each(function() {
+    var currentPage = 0;
+    var numPerPage = 10;
+    var $table = $(this);
+    $table.bind('repaginate', function() {
+        $table.find('.project-item').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
+    });
+    $table.trigger('repaginate');
+    var numRows = $table.find('.project-item').length;
+    var numPages = Math.ceil(numRows / numPerPage);
+    var $pager = $('<div class="pager"></div>');
+    for (var page = 0; page < numPages; page++) {
+        $('<span class="page-number"></span>').text(page + 1).bind('click', {
+            newPage: page
+        }, function(event) {
+            currentPage = event.data['newPage'];
+            $table.trigger('repaginate');
+            $(this).addClass('active').siblings().removeClass('active');
+        }).appendTo($pager).addClass('clickable');
+    }
+    $pager.insertBefore($table).find('span.page-number:first').addClass('active');
+});
+
 
 
 $(document).ready(function (){
